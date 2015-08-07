2c915ee42e06a99111de70d6effd79d16337de1f
Matt Fili <matthewjosephfili@gmail.com>
2015-08-07 11:43:13 -0500

    added moments and generated template from mongo collection

--- routes/cnode.js
+++ routes/cnode.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment')
+
 
 router.get('/', function (req, res) {
 	var collection = global.db.collection('cNode')
+
 	collection.find({}).toArray(function(err, order) {
 		var formattedOrders = order.map(function (order) {
 			return {
+				_idkey: 	order._id,
 				name: 		order.name,
 				style: 		order.style,
 				qty: 		order.qty,
-				createdAt: 	order._id.getTimestamp()
+				createdAt: 	moment(order._id.getTimestamp()).fromNow()
 			}
 		});
+		res.render('templates/cnode-index', {orders: formattedOrders})
 	});
-	res.render('templates/cnode-index', {orders: formattedOrders})
 });
 
 router.get('/order', function(req, res) {
@@ -22,8 +26,17 @@ router.get('/order', function(req, res) {
 
 router.post('/order', function (req, res) {
 	var collection = global.db.collection('cNode')
+
 	collection.save(req.body, function () {
-		res.redirect('/')
+		res.redirect('/cnode')
+	});
+});
+
+router.post('/order/:id/complete', function (req, res) {
+	var collection = global.db.collection('cNode')
+
+	collection.update({_id:req.params.id}, function () {
+		res.redirect('/cnode')
 	});
 });
 
