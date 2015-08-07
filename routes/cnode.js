e3af65d3713c765e4f2d22f4420df55ae7fbafe3
Matt Fili <matthewjosephfili@gmail.com>
2015-08-07 16:14:11 -0500

    queried mongo and rendered multiple view data

--- routes/cnode.js
+++ routes/cnode.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment')
+var ObjectId = require('mongodb').ObjectID
 
 
-router.get('/', function (req, res) {
+function getIncomplete(req, res, next) { 
 	var collection = global.db.collection('cNode')
 
-	collection.find({}).toArray(function(err, order) {
-		var formattedOrders = order.map(function (order) {
+	collection.find({ complete: { $exists: false} }).toArray(function(err, order) {
+		var incompleteOrders = order.map(function (order) {
 			return {
 				_idkey: 	order._id,
 				name: 		order.name,
@@ -16,9 +17,38 @@ router.get('/', function (req, res) {
 				createdAt: 	moment(order._id.getTimestamp()).fromNow()
 			}
 		});
-		res.render('templates/cnode-index', {orders: formattedOrders})
+		req.incomplete = incompleteOrders;
+		return next();
 	});
-});
+};
+
+function getComplete(req, res, next) { 
+	var collection = global.db.collection('cNode')
+	collection.find({ complete: { $exists: true} }).toArray(function(err, complete) {
+		var completeOrders = complete.map(function (order) {
+			return {
+				_idkey: 	order._id,
+				name: 		order.name,
+				style: 		order.style,
+				qty: 		order.qty,
+				createdAt: 	moment(order._id.getTimestamp()).fromNow()
+			}
+		});
+		req.complete = completeOrders;
+		return next();
+	});
+};
+
+function rendercNode (req, res) {
+	res.render('templates/cnode-index', {
+		orders : req.incomplete,
+		comOrders: req.complete
+	});
+};
+
+router.get('/', getIncomplete, getComplete, rendercNode)
+
+
 
 router.get('/order', function(req, res) {
 	res.render('templates/cnode-new');
@@ -34,9 +64,13 @@ router.post('/order', function (req, res) {
 
 router.post('/order/:id/complete', function (req, res) {
 	var collection = global.db.collection('cNode')
+	console.log(req.params.id)
 
-	collection.update({_id:req.params.id}, function () {
-		res.redirect('/cnode')
+	collection.update(
+		{_id: ObjectId(req.params.id)}, 
+		{$set: {complete: true}}, 
+		function () {
+			res.redirect('/cnode')
 	});
 });
 
