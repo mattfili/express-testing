70fe55845a486be09d2507d17eab3eb59493269e
Matt Fili <matthewjosephfili@gmail.com>
2015-08-09 17:34:12 -0500

    added mongodb to pizza

--- routes/pizza.js
+++ routes/pizza.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment')
+var ObjectId = require('mongodb').ObjectID
 
 
 router.get('/:topping/:qty', function (req, res) {
-	var obj = req.params;
-	obj.title = 'Pizza Shop';
-	
-	res.render('templates/pizza', obj)
+	var collection = global.db.collection('pizza')
+
+	collection.find({}).toArray(function(err, order) {
+		var pizzaOrder = order.map(function (order) {
+			return {
+				name:       'pizza',
+				topping: 	req.params.topping,
+				amount: 	req.params.qty 
+			}
+		});
+		res.render('templates/pizza', {pizza: pizzaOrder})
+	});
+});
+
+router.post('/:topping/:qty', function (req, res) {
+	var collection = global.db.collection('pizza')
+	console.log(req.params.topping)
+	console.log(req.params.qty)
+
+		collection.save({
+			name: 'pizza',
+			topping: req.params.topping,
+			amount: req.params.qty
+		});
+
 });
 
 module.exports = router;
